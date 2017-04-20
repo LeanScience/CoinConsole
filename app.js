de5376acd7748eb9b3855503a92de8016b6a6f2d
Michael Warner <michael@lumino.co>
2017-04-19 21:52:10 -0500

    app.js cleanup
    Did some overall cleaning of the server side script. Attempted to move
    the data retrieval system to another script as was done with routes,
    but since we are working with promises it doesn’t pass over data well
    through module.exports. Maybe we can look at it again in the future.
    
    Also segregated the bad words object to a .json file so it can be
    updated without restarting the server. This also cleaned up a nasty
    looking line of code! :)

--- app.js
+++ app.js
@@ -13,105 +13,25 @@ var http = require("http"),
 
     app = express();
 
+/*    DATA      */
 var globalDataOptions = {
-  "method": "GET",
-  "hostname": "api.coinmarketcap.com",
-  "port": null,
-  "path": "/v1/global/",
-  "headers": {
-    "cache-control": "no-cache"
-  }
-};
-
-var tickerDataOptions = {
-  "method": "GET",
-  "hostname": "api.coinmarketcap.com",
-  "port": null,
-  "path": "/v1/ticker/",
-  "headers": {
-    "cache-control": "no-cache"
-  }
-};
-
-var globalData,
-    tickerData,
-    userRefreshRate = 7.5; //number of seconds before data updates are sent to the client
-
-//listen for websocket connections
-websock.on('connection', onConnect);
-
-function onConnect(socket){
-  var badAttempt = 0,
-      messageAttempt = 0,
-      banned = false,
-
-      clearMessageAttemptTimer = 10; //number of seconds before the message count is reset
-
-  var messageAttemptTimer = setInterval(function(){
-    messageAttempt = 0;
-  }, clearMessageAttemptTimer * 1000);
-
-  socket.join('trollbox');
-  //send initial data
-  socket.emit('refreshTicker', tickerData);
-  socket.emit('refreshGlobal', globalData);
-
-  //ping the client to initiate the page
-  socket.emit('init', tickerData);
-
-  //refresh the data every 'userRefreshRate' seconds
-  var userRefreshTimer = setInterval(function() {
-    socket.emit('refreshTicker', tickerData);
-    socket.emit('refreshGlobal', globalData);
-  }, userRefreshRate * 1000);
-
-  socket.on('trollbox', function(data){
-    if (! socket.nickname){
-      ((data.name) ? socket.nickname = data.name : socket.nickname = "Anonymous");
-    }
-
-    if (data.message.length < 200 && messageAttempt < 10 && ! banned){
-      messageAttempt += 1;
-      filterBadWords(data.message).then(function(filteredMessage){
-        data.message = filteredMessage;
-        websock.to('trollbox').emit('trollbox', {"message": data.message, "name": socket.nickname});
-      });
-    } else {
-      socket.emit('trollbox', {"name": "CoinConsole", "message": "Nice try. Keep the trolling short and sweet."});
-      badAttempt += 1;
-      checkBadAttempts();
-    }
-  });
-
-  socket.on('disconnect', function() {
-    //Destroy the refresh timer for this user
-    clearInterval(userRefreshTimer);
-    clearInterval(messageAttemptTimer);
-    userRefreshTimer = null;
-    messageAttemptTimer = null;
-  });
-
-  function filterBadWords(s){
-    return new Promise(function(resolve, reject){
-      var badWords = ["fuck", "bastard", "cum", "cunt", "cock", "ass", "shit", "bitch"],
-          rgx = new RegExp(badWords.join("|"), "gi");
-
-      resolve(s.replace(rgx, "****"));
-    });
-  }
-
-  function checkBadAttempts(){
-    if (badAttempt >= 10) {
-      socket.emit('trollbox', {"name": "CoinConsole", "message": "You've been banned from the trollbox."});
-      ban();
-    }
-  }
-
-  function ban(){
-    socket.leave('trollbox');
-    banned = true;
-  }
-}
+      "method": "GET",
+      "hostname": "api.coinmarketcap.com",
+      "port": null,
+      "path": "/v1/global/",
+      "headers": {
+        "cache-control": "no-cache"
+      }
+    },
+    tickerDataOptions = {
+      "method": "GET",
+      "hostname": "api.coinmarketcap.com",
+      "port": null,
+      "path": "/v1/ticker/",
+      "headers": {
+        "cache-control": "no-cache"
+      }
+    };
 
 function getTickerData() {
   let cmcGET = new Promise(function(resolve, reject) {
@@ -182,21 +102,16 @@ function getGlobalData() {
 }
 
 //Get coin data on server start
-getTickerData();
-getGlobalData();
+var tickerData = getTickerData(),
+    globalData = getGlobalData();
 
-//Get coin data every minute - time will decrease at go-live
+//Get coin data every 15 seconds
 var autoRefresh = setInterval(function(){
-  getTickerData();
-  getGlobalData();
+  tickerData = getTickerData();
+  globalData = getGlobalData();
 }, 15 * 1000);
 
-/*    ROUTES    */
-//application page
-app.get('/', function(req, res){
-  res.render('index');
-});
-
+/*    GENERAL   */
 //let's use a templating engine...
 app.set('view engine', 'pug');
 app.set('views', cd + '/views');
@@ -204,5 +119,92 @@ app.set('views', cd + '/views');
 //oh yeah, we want our styles and scripts to be available, too...
 app.use('/assets', express.static(cd + '/assets'));
 
+//set the routes...
+var routes = require('./modules/routes');
+app.use(routes);
+
 //start the web server
 app.listen(webport);
+
+//listen for websocket client connections
+websock.on('connection', onConnect);
+
+function onConnect(socket){
+  var userRefreshRate = 7.5; //number of seconds before data updates are sent to the client
+
+  //send initial data
+  socket.emit('refreshTicker', tickerData);
+  socket.emit('refreshGlobal', globalData);
+
+  //ping the client to initiate the page
+  socket.emit('init', tickerData);
+
+  //refresh the data every 'userRefreshRate' seconds
+  var userRefreshTimer = setInterval(function() {
+    socket.emit('refreshTicker', tickerData);
+    socket.emit('refreshGlobal', globalData);
+  }, userRefreshRate * 1000);
+
+  socket.on('disconnect', function() {
+    //Destroy the refresh timer for this user
+    clearInterval(userRefreshTimer);
+    clearInterval(messageAttemptTimer);
+    userRefreshTimer = null;
+    messageAttemptTimer = null;
+  });
+
+  /*   Trollbox functions   */
+  var badAttempt = 0,
+      messageAttempt = 0,
+      banned = false,
+
+      clearMessageAttemptTimer = 10; //number of seconds before the message count is reset
+
+  var messageAttemptTimer = setInterval(function(){
+    messageAttempt = 0;
+  }, clearMessageAttemptTimer * 1000);
+
+  socket.join('trollbox');
+
+  socket.on('trollbox', function(data){
+    if (! socket.nickname){
+      ((data.name && data.name.length <= 16) ? filterBadWords(data.name).then(function(filteredName){socket.nickname = filteredName}) : socket.nickname = "anonymous");
+    }
+
+    if (data.message.length < 200 && data.message.length > 0 && messageAttempt <= 3 && ! banned){
+      messageAttempt += 1;
+      filterBadWords(data.message).then(function(filteredMessage){
+        data.message = filteredMessage;
+        websock.to('trollbox').emit('trollbox', {"message": data.message, "name": socket.nickname});
+      });
+    } else {
+      if (!banned){
+        socket.emit('trollbox', {"name": "CoinConsole", "message": "Nice try. Keep the trolling short and sweet."});
+        badAttempt += 1;
+        checkBadAttempts();
+      } else {
+        socket.emit('trollbox', {"name": "CoinConsole", "message": "You've been banned from the trollbox."});
+      }
+    }
+  });
+
+  function filterBadWords(s){
+    return new Promise(function(resolve, reject){
+      var badWords = ["fuck", "bastard", "cum", "cunt", "cock", "ass", "shit", "bitch"], //require('./modules/badwords')
+          rgx = new RegExp(badWords.join("|"), "gi");
+
+      resolve(s.replace(rgx, "****"));
+    });
+  }
+
+  function checkBadAttempts(){
+    if (badAttempt >= 10) {
+      ban();
+    }
+  }
+
+  function ban(){
+    socket.leave('trollbox');
+    banned = true;
+  }
+}
