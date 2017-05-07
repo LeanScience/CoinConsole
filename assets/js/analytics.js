1ebce014aadb332c921ce65529e5c726e0b7867a
Michael Warner <michael@lumino.co>
2017-05-06 20:13:13 -0500

    Added Google Analytics
    Added GA so we can use semrush.com to improve SEO when the time is
    right.

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -10,3 +10,12 @@ _paq.push(['enableLinkTracking']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
+
+//Google analytics tracking code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-98643985-1', 'auto');
+ga('send', 'pageview');
